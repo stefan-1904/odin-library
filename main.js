0db9a91ead38e8a81364a85367be16777ba6232a
Stefan <sminns1230@gmail.com>
2024-02-18 11:46:45 +0000

    Add form functionality

--- main.js
+++ main.js
@@ -5,5 +5,19 @@ function Book() {
 }
 
 function addBookToLibrary() {
-    
+    const addBook = document.getElementById('newBook');
+    const addBookForm = document.getElementById('bookEntryForm');
+    addBook.addEventListener('click', () => {
+        addBook.style.visibility = 'hidden';
+        addBookForm.style.visibility = 'visible';
+    })
 }
+
+addBookToLibrary()
+
+function bookDisplay() {
+    myLibrary.forEach((book) => {
+        console.log(book)
+    })
+}
+
